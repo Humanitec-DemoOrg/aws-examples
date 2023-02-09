b6736655c59908bbb3a012f950ffe8cf43685f82
nickhumanitec <121984975+nickhumanitec@users.noreply.github.com>
2023-02-09 10:45:43 -0700

    allow assume role

--- s3/terraform/bucket/main.tf
+++ s3/terraform/bucket/main.tf
@@ -19,6 +19,7 @@ output "aws_access_key_id" {
   value     = ""
   sensitive = true
 }
+
 output "aws_secret_access_key" {
   value     = ""
   sensitive = true
@@ -28,6 +29,10 @@ output "aws_secret_access_key" {
 variable "region" {}
 variable "access_key" {}
 variable "secret_key" {}
+
+variable "assume_role_arn" {}
+variable "assume_role_session_name" {}
+variable "assume_role_external_id" {}
 
 terraform {
   required_providers {
@@ -41,6 +46,11 @@ provider "aws" {
   region     = var.region
   access_key = var.access_key
   secret_key = var.secret_key
+  assume_role {
+    role_arn     = var.assume_role_arn
+    session_name = var.assume_role_session_name
+    external_id  = var.assume_role_external_id
+  }
 }
 
 data "aws_caller_identity" "current" {}
