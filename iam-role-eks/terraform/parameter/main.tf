cf23a108476330ac22bf90ea2d74ed0c7c14c556
nickhumanitec <121984975+nickhumanitec@users.noreply.github.com>
2023-02-27 10:05:43 -0700

    Role for tf eks (#9)
    * Update main.tf
    
    * Update main.tf
    
    * Update main.tf
    
    * Update main.tf

--- iam-role-eks/terraform/parameter/main.tf
+++ iam-role-eks/terraform/parameter/main.tf
@@ -28,7 +28,7 @@ output "parameter_id" {
 variable "region" {}
 variable "access_key" {}
 variable "secret_key" {}
-
+variable "assume_role_arn" {}
 terraform {
   required_providers {
     aws = {
@@ -41,6 +41,9 @@ provider "aws" {
   region     = var.region
   access_key = var.access_key
   secret_key = var.secret_key
+  assume_role {
+    role_arn = var.assume_role_arn
+  }
 }
 
 data "aws_caller_identity" "current" {}
