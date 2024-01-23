ffdaf3a5df05bd52d171c53e510d2a30e03171ba
nickhumanitec <121984975+nickhumanitec@users.noreply.github.com>
2024-01-22 17:09:37 -0700

    s3ro policy

---
+++ pod-identities/resource-definition/source/s3ro-policy.tf
@@ -0,0 +1,38 @@
+variable "app" {}
+variable "env" {}
+variable "res" {}
+variable "name" {}
+variable "arns" {}
+
+locals {
+  name = substr(replace(replace(replace(replace(var.name, "modules.", ""), "shared.", "-s-"), "externals.", ""), ".", ""), 0, 63)
+}
+output "arn" {
+  value = aws_iam_policy.this.arn
+}
+
+variable "region" {}
+variable "access_key" {}
+variable "secret_key" {}
+
+provider "aws" {
+  region     = var.region
+  access_key = var.access_key
+  secret_key = var.secret_key
+}
+
+resource "aws_iam_policy" "this" {
+  name_prefix = local.name
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        "Effect" : "Allow",
+        "Action" : [
+          "s3:GetObject"
+        ],
+        "Resource" : flatten([for s in var.arns : split(",", format("%s,%s/*", s, s))])
+      }
+    ]
+  })
+}
